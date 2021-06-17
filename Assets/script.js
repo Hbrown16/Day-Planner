13e67b69570fdfab1d8eabc90ac4173fd837ea91
Hbrown16 <hbrown1094@gmail.com>
2021-06-17 18:25:31 -0400

    update

--- Assets/script.js
+++ Assets/script.js
@@ -28,72 +28,25 @@ $(".time-slot").each(function(){
 //Save Button/Event Listener and Function
 $(document).on('click',"saveBtn", function(event) {
   event.preventDefault();
-  var value = $(this).siblings("description").val();
+  var hour = $(this).siblings("description").val();
   var times = $(this).parent().attr("id");
-  localStorage.setItem(times,value);
+  localStorage.setItem(times,hour);
 })
-$(document).ready(function () {
-    // listen for save button clicks
-    $('.saveBtn').on('click', function () {
-      // get nearby values
-      var value = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('id');
+
+// load any saved data from localStorage
+$('#hour-6 .description').val(localStorage.getItem('hour-6'));
+$('#hour-7 .description').val(localStorage.getItem('hour-7'));
+$('#hour-8 .description').val(localStorage.getItem('hour-8'));
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-1 .description').val(localStorage.getItem('hour-1'));
+$('#hour-2 .description').val(localStorage.getItem('hour-2'));
+$('#hour-3 .description').val(localStorage.getItem('hour-3'));
+$('#hour-4 .description').val(localStorage.getItem('hour-4'));
+$('#hour-5 .description').val(localStorage.getItem('hour-5'));
+$('#hour-6p .description').val(localStorage.getItem('hour-6p'));
   
-      // save in localStorage
-      localStorage.setItem(time, value);
   
-      // Show notification that item was saved to localStorage by adding class 'show'
-      $('.notification').addClass('show');
-  
-      // Timeout to remove 'show' class after 5 seconds
-      setTimeout(function () {
-        $('.notification').removeClass('show');
-      }, 5000);
-    });
-  
-    function hourUpdater() {
-      // get current number of hours
-      var currentHour = moment().hours();
-  
-      // loop over time blocks
-      $('.time-block').each(function () {
-        var blockHour = parseInt($(this).attr('id').split('-')[1]);
-  
-        // check if we've moved past this time
-        if (blockHour < currentHour) {
-          $(this).addClass('past');
-        } else if (blockHour === currentHour) {
-          $(this).removeClass('past');
-          $(this).addClass('present');
-        } else {
-          $(this).removeClass('past');
-          $(this).removeClass('present');
-          $(this).addClass('future');
-        }
-      });
-    }
-  
-    hourUpdater();
-  
-    // set up interval to check if current time needs to be updated
-    var interval = setInterval(hourUpdater, 15000);
-  
-    // load any saved data from localStorage
-    $('#hour-6 .description').val(localStorage.getItem('hour-6'));
-    $('#hour-7 .description').val(localStorage.getItem('hour-7'));
-    $('#hour-8 .description').val(localStorage.getItem('hour-8'));
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-1 .description').val(localStorage.getItem('hour-1'));
-    $('#hour-2 .description').val(localStorage.getItem('hour-2'));
-    $('#hour-3 .description').val(localStorage.getItem('hour-3'));
-    $('#hour-4 .description').val(localStorage.getItem('hour-4'));
-    $('#hour-5 .description').val(localStorage.getItem('hour-5'));
-    $('#hour-6p .description').val(localStorage.getItem('hour-6p'));
-  
-    // display current day on page
-    $('#currentDay').text(moment().format('dddd, MMMM Do'));
-  });
   
